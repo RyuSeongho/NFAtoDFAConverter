026cd982a940cdd66f88ac243620f398b7862a16
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-05-16 03:43:56 +0900

    feat: added main Converter

---
+++ Converter.py
@@ -0,0 +1,16 @@
+import FiniteAutomata
+import os
+import glob
+
+def main():
+    file_path = "C:\\Users\\ryuse\\Desktop\\류성호\\동국대학교\\3학년\\1학기\\형식언어\\HW2\\NFA"
+    files = glob.glob(os.path.join(file_path, "*.txt"))
+
+    for file in files:
+        fa = FiniteAutomata.FiniteAutomata(file)
+        print(os.path.basename(file))
+        print(fa.__str__() + "\n")
+
+
+if __name__ == "__main__":
+    main()
